c671a0ae1a8af4eee532e5db90d0ebc89a594815
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-04 18:59:22 -0400

    Apply eslint fix

--- src/components/svelte/story-player/components/story.svelte
+++ src/components/svelte/story-player/components/story.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
   import { videoSourcesStore, videoPlayingIndexStore } from '../store';
 
-	export let story: StoryPlayer.Story;
-	
+  export let story: StoryPlayer.Story;
+
   const handleChangeSource = (storySources: StoryPlayer.Source[]) => {
-    videoSourcesStore.set(storySources)
-		videoPlayingIndexStore.set(0)
-  }
+    videoSourcesStore.set(storySources);
+    videoPlayingIndexStore.set(0);
+  };
 </script>
 
 <li class="cursor-pointer" on:click={() => handleChangeSource(story.sources)}>
-	<figure class="flex flex-col items-center">
-		<div class="animated-gradient rounded-full w-16 h-16 p-[4px]">
-			<div class="bg-slate-900 rounded-full h-full w-full p-2">
-				<img class="h-full w-full" src={story.image} alt={story.label}>
-			</div>
-		</div>
-		<figcaption class="text-center text-xs text-gray-800 mt-2 w-max" style="color: #f2f2f2">
-			{story.label}
-		</figcaption>
-	</figure>
+  <figure class="flex flex-col items-center">
+    <div class="animated-gradient rounded-full w-16 h-16 p-[4px]">
+      <div class="bg-slate-900 rounded-full h-full w-full p-2">
+        <img class="h-full w-full" src={story.image} alt={story.label} />
+      </div>
+    </div>
+    <figcaption class="text-center text-xs text-gray-800 mt-2 w-max" style="color: #f2f2f2">
+      {story.label}
+    </figcaption>
+  </figure>
 </li>
-
