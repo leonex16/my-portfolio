7cf7a51b8842c9946facda7d75b237f4ecb8bdef
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-04 18:50:03 -0400

    Implement loader component on story-player

--- src/components/svelte/story-player/index.svelte
+++ src/components/svelte/story-player/index.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-  import { videoSourcesStore } from './store';
-  import storiesData from '../../../data/story-player-stories.json';
-
+  import { videoSourcesStore, isLoadingVideoStore } from './store';
+  
+  import Loader from '../loader/index.svelte';
   import ProgressBar from './components/progress-bar.svelte';
-  import Loader from './components/loader.svelte';
   import SpeakerButton from './components/speaker-button.svelte';
   import Story from './components/story.svelte';
   import Video from './components/video.svelte';
+
+  import storiesData from '../../../data/story-player-stories.json';
 
   let [_, ...stories] = storiesData;
 </script>
 
 <article class="relative">
-  <Loader />
   <SpeakerButton />
 
   <div
-    class="relative shadow-lg shadow-stale-400/50 bg-slate-800 rounded-xl w-screen overflow-hidden aspect-[9/16] max-w-[400px]"
+    class="relative shadow-stale-400/20 shadow-lg rounded-xl w-screen overflow-hidden aspect-[9/16] max-w-[400px]"
   >
+    <Loader isLoading={$isLoadingVideoStore} />
     <header class="relative flex gap-2 p-2 z-[var(--z-index-story-player-progress-bars)]">
       {#each $videoSourcesStore as source}
         <ProgressBar bind:progressBarRef={source.progressBarRef} />
