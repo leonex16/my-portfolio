0e2f34125788602f80aaec0747b2cfc9320de052
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-03 22:37:55 -0400

    Add story componet for story-player

---
+++ src/components/svelte/story-player/components/story.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { videoSourcesStore, videoPlayingIndexStore } from '../store';
+
+	export let story: StoryPlayer.Story;
+	
+  const handleChangeSource = (storySources: StoryPlayer.Source[]) => {
+    videoSourcesStore.set(storySources)
+		videoPlayingIndexStore.set(0)
+  }
+</script>
+
+<li class="cursor-pointer" on:click={() => handleChangeSource(story.sources)}>
+	<figure class="flex flex-col items-center">
+		<div class="bg-gradient-to-r from-green-500 to-blue-500 rounded-full w-14 h-14 p-1">
+			<div class="bg-white rounded-full h-full w-full p-2">
+				<img class="h-full w-full" src={story.image} alt={story.label}>
+			</div>
+		</div>
+		<figcaption class="text-center text-xs text-gray-800 mt-2 w-max">
+			{story.label}
+		</figcaption>
+	</figure>
+</li>
+
