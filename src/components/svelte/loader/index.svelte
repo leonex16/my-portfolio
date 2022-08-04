af51a56991d7b852e49d49e6133dfc0de5d49fa0
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-04 18:49:09 -0400

    Move loader component from story-player to its own folder

---
+++ src/components/svelte/loader/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let isLoading = false;
+</script>
+
+<span
+  class={`
+  loader
+  z-[var(--z-index-story-player-loader)]
+  ${isLoading ? 'inline-block' : 'hidden'}
+`}
+/>
+
+<style>
+  .loader {
+    position: absolute;
+    inset: 0;
+    margin: auto;
+    width: 48px;
+    height: 48px;
+    border: 5px solid #fff;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
