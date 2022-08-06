ac0307030e43884b3c73c0c8d59c96f4bfa91b03
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-05 20:41:26 -0400

    Fix tailwind file name and add xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const { screens } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{html,astro,ts,svelte}'],
   theme: {
+    screens: {
+      xs: '320px',
+      ...screens
+    },
     extend: {
       fontFamily: {
         monserrat: ['Montserrat', 'sans-serif']
