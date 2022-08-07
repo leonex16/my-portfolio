3c1e6db4f1d0a2b6bf9922a8ea286dadbc97734e
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-07 02:06:19 -0400

    Rename variable

--- tests/e2e/portfolio-view.test.ts
+++ tests/e2e/portfolio-view.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 const MS_TO_WAIT_CHANGE_STATE_SRC = 2000
 
-test.describe('StoryPlayer', () => {
+test.describe('Portfolio', () => {
   test.skip(({ isMobile }) => Boolean(isMobile), 'Portfolio-view only runs in desktop resolutions')
 
   test.beforeEach(async ({ page }) => {
@@ -10,36 +10,36 @@ test.describe('StoryPlayer', () => {
   })
 
   test('should video progress bar to be empty when starting to load the video', async ({ page }) => {
-    const msgErrorExpected = 'The video progress bar expected to be empty when starting to load the video'
+    const errorMsgExpected = 'The video progress bar expected to be empty when starting to load the video'
     const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
 
-    expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 0%')
+    expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toBe('width: 0%')
   })
 
   test('should video source changes when the user pressed the next button', async ({ page }) => {
-    const msgErrorExpected = 'Video source expected to change when the user press the next button'
+    const errorMsgExpected = 'Video source expected to change when the user press the next button'
     const firstVideoUrl = await page.locator('video >> nth=0').getAttribute('src')
     const nextBtn = page.locator('button >> nth=1')
 
     await nextBtn.click()
     await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
 
-    expect(await page.locator('video >> nth=0').getAttribute('src'), msgErrorExpected).not.toBe(firstVideoUrl)
+    expect(await page.locator('video >> nth=0').getAttribute('src'), errorMsgExpected).not.toBe(firstVideoUrl)
   })
 
   test('should previous video progress bar to be full when press next button', async ({ page, browserName }) => {
-    const msgErrorExpected = 'Previous video progress bar expected to be full when the user has pressed pressed the next button'
+    const errorMsgExpected = 'Previous video progress bar expected to be full when the user has pressed pressed the next button'
     const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
     const nextBtn = page.locator('button >> nth=1')
 
     await nextBtn.click()
     await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
 
-    expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 100%;')
+    expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toBe('width: 100%;')
   })
 
   test('should video source to be same that first video source when the user pressed the previous button', async ({ page }) => {
-    const msgErrorExpected = 'Video source expected to be same that first video source when the user has pressed the previous button'
+    const errorMsgExpected = 'Video source expected to be same that first video source when the user has pressed the previous button'
     const firstVideoUrl = await page.locator('video >> nth=0').getAttribute('src')
     const prevBtn = page.locator('button >> nth=0')
     const nextBtn = page.locator('button >> nth=1')
@@ -49,29 +49,29 @@ test.describe('StoryPlayer', () => {
     await prevBtn.click()
     await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
 
-    expect(await page.locator('video >> nth=0').getAttribute('src'), msgErrorExpected).toBe(firstVideoUrl)
+    expect(await page.locator('video >> nth=0').getAttribute('src'), errorMsgExpected).toBe(firstVideoUrl)
   })
 
   test('should the first progress bar begins from zero when finished the stories', async ({ page }) => {
-    const msgErrorExpected = 'The first progress bar expected to begin from zero when finished the stories'
+    const errorMsgExpected = 'The first progress bar expected to begin from zero when finished the stories'
     const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
     const nextBtn = page.locator('button >> nth=1')
     const totalVideos = await page.locator('astro-island header').evaluate(header => header.children.length)
 
     await nextBtn.click({ clickCount: totalVideos, delay: 500 })
 
-    expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 0%;')
+    expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toBe('width: 0%;')
   })
 
   test('should video to be muted', async ({ page }) => {
-    const msgErrorExpected = 'Video expected to be muted on page load'
+    const errorMsgExpected = 'Video expected to be muted on page load'
 
-    await expect(page.locator('.speaker >> nth=0'), msgErrorExpected).not.toBeVisible()
+    await expect(page.locator('.speaker >> nth=0'), errorMsgExpected).not.toBeVisible()
   })
 
   test('should video to be unmuted', async ({ page }) => {
-    const msgErrorExpected = 'Video expected to be unmuted when the user has pressed the button'
+    const errorMsgExpected = 'Video expected to be unmuted when the user has pressed the button'
 
-    await expect(page.locator('.speaker--muted >> nth=0'), msgErrorExpected).toBeVisible()
+    await expect(page.locator('.speaker--muted >> nth=0'), errorMsgExpected).toBeVisible()
   })
 })
