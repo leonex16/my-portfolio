2040aa3aa22d4d00aa9f4249c87dfd572d688666
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-07 00:40:56 -0400

    Refactor portfolio tests

--- tests/e2e/portfolio-view.test.ts
+++ tests/e2e/portfolio-view.test.ts
@@ -1,47 +1,77 @@
 import { test, expect } from '@playwright/test'
 
-test('StoryPlayer', async ({ page }) => {
-  let msgErrorExpected = ''
+const MS_TO_WAIT_CHANGE_STATE_SRC = 2000
 
-  await page.goto('/')
+test.describe('StoryPlayer', () => {
+  test.skip(({ isMobile }) => Boolean(isMobile), 'Portfolio-view only runs in desktop resolutions')
 
-  const totalVideos = await page.locator('astro-island header').evaluate(header => header.children.length)
-  const firstVideoUrl = await page.locator('video >> nth=0').getAttribute('src')
-  const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
-  const speakerBtn = page.locator('.speaker--muted >> nth=0')
-  const prevBtn = page.locator('button >> nth=0')
-  const nextBtn = page.locator('button >> nth=1')
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
 
-  // Test 1
-  msgErrorExpected = 'Expected that video progress bar to be empty when starting to load the video'
-  expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 0%')
+  test('should video progress bar to be empty when starting to load the video', async ({ page }) => {
+    const msgErrorExpected = 'The video progress bar expected to be empty when starting to load the video'
+    const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
 
-  // Test 2
-  msgErrorExpected = 'Expected that video source changes when the user pressed the next button'
-  await nextBtn.click()
-  expect(await page.locator('video >> nth=0').getAttribute('src'), msgErrorExpected).not.toBe(firstVideoUrl)
+    expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 0%')
+  })
 
-  // Test 3
-  msgErrorExpected = 'Expected that previous video progress bar to be full when press next button'
-  await page.waitForTimeout(800)
-  expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 100%;')
+  test('should video source changes when the user pressed the next button', async ({ page }) => {
+    const msgErrorExpected = 'Video source expected to change when the user press the next button'
+    const firstVideoUrl = await page.locator('video >> nth=0').getAttribute('src')
+    const nextBtn = page.locator('button >> nth=1')
 
-  // Test 4
-  msgErrorExpected = 'Expected that video source to be same that first video source when the user pressed the previous button'
-  await prevBtn.click()
-  expect(await page.locator('video >> nth=0').getAttribute('src'), msgErrorExpected).toBe(firstVideoUrl)
+    await nextBtn.click()
+    await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
 
-  // Test 5
-  msgErrorExpected = 'Expected that the first progress bar begins from zero when finished the stories'
-  await nextBtn.click({ clickCount: totalVideos, delay: 500 })
-  expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 0%;')
+    expect(await page.locator('video >> nth=0').getAttribute('src'), msgErrorExpected).not.toBe(firstVideoUrl)
+  })
 
-  // Test 6
-  msgErrorExpected = 'Expected video to be muted'
-  await expect(page.locator('.speaker >> nth=0'), msgErrorExpected).not.toBeVisible()
+  test('should previous video progress bar to be full when press next button', async ({ page, browserName }) => {
+    const msgErrorExpected = 'Previous video progress bar expected to be full when the user has pressed pressed the next button'
+    const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
+    const nextBtn = page.locator('button >> nth=1')
 
-  // Test 7
-  msgErrorExpected = 'Expected video to be unmuted'
-  await speakerBtn.click()
-  await expect(page.locator('.speaker--muted >> nth=0'), msgErrorExpected).toBeVisible()
+    await nextBtn.click()
+    await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
+
+    expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 100%;')
+  })
+
+  test('should video source to be same that first video source when the user pressed the previous button', async ({ page }) => {
+    const msgErrorExpected = 'Video source expected to be same that first video source when the user has pressed the previous button'
+    const firstVideoUrl = await page.locator('video >> nth=0').getAttribute('src')
+    const prevBtn = page.locator('button >> nth=0')
+    const nextBtn = page.locator('button >> nth=1')
+
+    await nextBtn.click()
+    await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
+    await prevBtn.click()
+    await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
+
+    expect(await page.locator('video >> nth=0').getAttribute('src'), msgErrorExpected).toBe(firstVideoUrl)
+  })
+
+  test('should the first progress bar begins from zero when finished the stories', async ({ page }) => {
+    const msgErrorExpected = 'The first progress bar expected to begin from zero when finished the stories'
+    const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
+    const nextBtn = page.locator('button >> nth=1')
+    const totalVideos = await page.locator('astro-island header').evaluate(header => header.children.length)
+
+    await nextBtn.click({ clickCount: totalVideos, delay: 500 })
+
+    expect(await firstVideoProgressBar.getAttribute('style'), msgErrorExpected).toBe('width: 0%;')
+  })
+
+  test('should video to be muted', async ({ page }) => {
+    const msgErrorExpected = 'Video expected to be muted on page load'
+
+    await expect(page.locator('.speaker >> nth=0'), msgErrorExpected).not.toBeVisible()
+  })
+
+  test('should video to be unmuted', async ({ page }) => {
+    const msgErrorExpected = 'Video expected to be unmuted when the user has pressed the button'
+
+    await expect(page.locator('.speaker--muted >> nth=0'), msgErrorExpected).toBeVisible()
+  })
 })
