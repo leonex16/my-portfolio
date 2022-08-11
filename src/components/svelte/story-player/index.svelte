653293fd71135855a853c944a68dad0dbb7db53f
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-11 02:50:12 -0400

    Implement functionality to play-pause-button component

--- src/components/svelte/story-player/index.svelte
+++ src/components/svelte/story-player/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { videoSourcesStore, isLoadingVideoStore } from './store';
-  
+  import { videoSourcesStore, isVideoLoadingStore } from './store';
+
   import Loader from '../loader/index.svelte';
   import ProgressBar from './components/progress-bar.svelte';
+  import PlayPauseButton from './components/play-pause-button.svelte';
   import SpeakerButton from './components/speaker-button.svelte';
   import Story from './components/story.svelte';
   import Video from './components/video.svelte';
@@ -15,10 +16,9 @@
 <article class="relative w-screen max-w-[280px] xl:max-w-[320px] 2xl:max-w-[412px]">
   <SpeakerButton />
 
-  <div
-    class="relative shadow-stale-400/20 shadow-lg rounded-xl overflow-hidden aspect-[9/16]"
-  >
-    <Loader isLoading={$isLoadingVideoStore} />
+  <div class="relative shadow-stale-400/20 shadow-lg rounded-xl overflow-hidden aspect-[9/16]">
+    <Loader isLoading={$isVideoLoadingStore} />
+    <PlayPauseButton />
     <header class="relative flex gap-2 p-2 z-[var(--z-index-story-player-progress-bars)]">
       {#each $videoSourcesStore as source}
         <ProgressBar bind:progressBarRef={source.progressBarRef} />
--- src/components/svelte/story-player/store.ts
+++ src/components/svelte/story-player/store.ts
@@ -4,6 +4,7 @@ import { writable } from 'svelte/store'
 import stories from '../../../data/story-player-stories.json'
 
 export const isMutedAudioStore = writable(true)
-export const isLoadingVideoStore = writable(false)
+export const isVideoPlayingStore = writable(true)
+export const isVideoLoadingStore = writable(false)
 export const videoPlayingIndexStore = writable(0)
 export const videoSourcesStore = writable<StoryPlayer.Source[]>(stories[0].sources)
