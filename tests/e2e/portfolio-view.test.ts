b26c8a2cc4b4e1291d80c54983c0d62515973da7
Leonel Espinoza Sotelo <leonelantonio569@gmail.com>
2022-08-12 01:58:59 -0400

    Add tests for story feature

--- tests/e2e/portfolio-view.test.ts
+++ tests/e2e/portfolio-view.test.ts
@@ -9,37 +9,6 @@ test.describe('Portfolio', () => {
     await page.goto('/')
   })
 
-  test.describe('Video Progress Bar', () => {
-    test('should be empty when starting to load the video', async ({ page }) => {
-      const errorMsgExpected = 'The video progress bar expected to be empty when starting to load the video'
-      const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
-
-      expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toBe('width: 0%')
-    })
-
-    test('previous should be full when press next button', async ({ page, browserName }) => {
-      const errorMsgExpected = 'Previous video progress bar expected to be full when the user has pressed pressed the next button'
-      const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
-      const nextBtn = page.locator('button >> nth=1')
-
-      await nextBtn.click()
-      await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
-
-      expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toBe('width: 100%;')
-    })
-
-    test('should the first progress bar begins from zero when finished the stories', async ({ page }) => {
-      const errorMsgExpected = 'The first progress bar expected to begin from zero when finished the stories'
-      const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
-      const nextBtn = page.locator('button >> nth=1')
-      const totalVideos = await page.locator('astro-island header').evaluate(header => header.children.length)
-
-      await nextBtn.click({ clickCount: totalVideos, delay: 500 })
-
-      expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toBe('width: 0%;')
-    })
-  })
-
   test.describe('Previous/Next StoryPlayer Buttons', () => {
     test('should video source changes when the user pressed the next button', async ({ page }) => {
       const errorMsgExpected = 'Video source expected to change when the user press the next button'
@@ -64,6 +33,37 @@ test.describe('Portfolio', () => {
       await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
 
       expect(await page.locator('video >> nth=0').getAttribute('src'), errorMsgExpected).toBe(firstVideoUrl)
+    })
+  })
+
+  test.describe('Video Progress Bar', () => {
+    test('should be empty when starting to load the video', async ({ page }) => {
+      const errorMsgExpected = 'The video progress bar expected to be empty when starting to load the video'
+      const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
+
+      expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toMatch('width: 0%')
+    })
+
+    test('previous should be full when press next button', async ({ page, browserName }) => {
+      const errorMsgExpected = 'Previous video progress bar expected to be full when the user has pressed pressed the next button'
+      const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
+      const nextBtn = page.locator('button >> nth=1')
+
+      await nextBtn.click()
+      await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
+
+      expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toMatch('width: 100%;')
+    })
+
+    test('should the first progress bar begins from zero when finished the stories', async ({ page }) => {
+      const errorMsgExpected = 'The first progress bar expected to begin from zero when finished the stories'
+      const firstVideoProgressBar = page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
+      const nextBtn = page.locator('button >> nth=1')
+      const totalVideos = await page.locator('astro-island header').evaluate(header => header.children.length)
+
+      await nextBtn.click({ clickCount: totalVideos, delay: 500 })
+
+      expect(await firstVideoProgressBar.getAttribute('style'), errorMsgExpected).toMatch('width: 0%;')
     })
   })
 
@@ -102,7 +102,7 @@ test.describe('Portfolio', () => {
       // const isPaused = await video.evaluate(($video: HTMLVideoElement) => $video.paused)
 
       // expect(isPaused).toBe(false)
-      await expect(page.locator('.play-pause-icon'), errorMsgExpected).not.toBeVisible()
+      await expect(page.locator('.play-pause-icon'), errorMsgExpected).toHaveCSS('opacity', '0')
     })
 
     test('should video pause when the user clicks', async ({ page }) => {
@@ -117,7 +117,32 @@ test.describe('Portfolio', () => {
       // isPaused = await video.evaluate(($video: HTMLVideoElement) => $video.paused)
 
       // expect(isPaused).toBe(true)
-      await expect(playPauseButton, errorMsgExpected).toBeVisible()
+      await expect(playPauseButton, errorMsgExpected).toHaveCSS('opacity', '1')
+    })
+  })
+
+  test.describe('Stories', () => {
+    test('should switch stories when the user clicks on the another story', async ({ page }) => {
+      const errorMsgExpected = 'Sources expected to switch when the user clicks on the another story'
+      const firstVideoUrl = await page.locator('video >> nth=0').getAttribute('src')
+      const storyBtn = page.locator('figure:has-text("Sobre Mi")')
+
+      await storyBtn.click()
+      await page.waitForTimeout(MS_TO_WAIT_CHANGE_STATE_SRC)
+
+      expect(await page.locator('video >> nth=0').getAttribute('src'), errorMsgExpected).not.toBe(firstVideoUrl)
+    })
+
+    test('progress bar should begin to zero when the user clicks on the another story', async ({ page }) => {
+      const errorMsgExpected = 'Progress bar expected empty when the user clicks on the another story'
+      const storyBtn = page.locator('figure:has-text("Sobre Mi")')
+      const nextBtn = page.locator('button >> nth=1')
+      const firstVideoProgressBar = () => page.locator('div:nth-child(2) > .overflow-hidden > .bg-slate-100 >> nth=0')
+
+      await nextBtn.click()
+      await storyBtn.click()
+
+      expect(await firstVideoProgressBar().getAttribute('style'), errorMsgExpected).toMatch('width: 0%')
     })
   })
 })
